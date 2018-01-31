0a9f2f29a33637d8f6c44cd8d6f6790213f9aeff
Huw Pritchard <huw008@gmail.com>
2018-01-31 21:30:51 +0000

    Added Nameable class

--- source/functions/array.cpp
+++ source/functions/array.cpp
@@ -20,7 +20,7 @@ namespace HuwInterpreter {
         Array::Array(std::shared_ptr<Passable> passable)
             : Function(passable)
         {
-            name = "array";
+            setName("array");
         }
 
         std::shared_ptr<Variable> Array::execute(std::shared_ptr<Tokens::Token> token, std::shared_ptr<Scope> globalScope,
